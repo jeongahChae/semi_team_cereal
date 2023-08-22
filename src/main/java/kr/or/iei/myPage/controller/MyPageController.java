e7a57f3fda1a2c1f578d239b2d12354e066586da
Donghyo-Son <121651986+Donghyo-Son@users.noreply.github.com>
2023-08-22 10:41:24 +0900

    css 디자인 변경

---
+++ src/main/java/kr/or/iei/myPage/controller/MyPageController.java
@@ -0,0 +1,16 @@
+package kr.or.iei.myPage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="myPage")
+public class MyPageController {
+	
+	@GetMapping(value="/cssTest")
+	public String orderHistoryDeliveryStatus1() {
+		return "myPage/Donghyo_orderCancel-change-return_3";
+	}//orderHistoryDeliveryStatus1()
+	
+}//MyPageController
