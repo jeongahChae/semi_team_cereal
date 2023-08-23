a4af2a6901471b863ad7ccc64d05fd64dd5612b1
chae <142170487+pondue@users.noreply.github.com>
2023-08-23 18:35:26 +0900

    비동기 페이징 angry

--- src/main/resources/static/javascript/eventList.js
+++ src/main/resources/static/javascript/eventList.js
@@ -9,5 +9,72 @@ eventTabs.on("click",function(){
     eventTabs.removeClass("event-active");
     eventTabs.eq(index).addClass("event-active");
     eventContents.hide();
+    $(".winnerTr").empty();
+    $("#pageNavi").empty();
     eventContents.eq(index).show();
+
+    let reqPage = 1;
+    $.ajax({
+        url : "/event/winnerBoard",
+        type : "get",
+        data : {reqPage : reqPage},
+        success : function(data){
+            for(let i=0 ; i<data.winnerBoardList.length ; i++){
+                const winnerBoard = data.winnerBoardList[i];
+                const tr = $("<tr>");
+                tr.addClass("winnerTr")
+                const noTd = $("<td>");
+                noTd.text(winnerBoard.winNo);
+                const titleTd = $("<td>");
+                titleTd.text(winnerBoard.winTitle);
+                const writerTd = $("<td>");
+                writerTd.text(winnerBoard.winWriter);
+                const regDateTd = $("<td>");
+                regDateTd.text(winnerBoard.regDate);
+                tr.append(noTd).append(titleTd).append(writerTd).append(regDateTd);
+                $(".notice-tbl").children("tbody").append(tr);
+            }
+            $("#pageNavi").append(data.pageNavi);
+            const pageTabs = $("#pageNavi>ul>li>a");
+            pageTabs.on("click",function(){
+                
+            });
+        }
+    });
 });
+
+const pageTabs = $("#pageNavi>ul>li>a");
+pageTabs.on("click",function(){
+    $(".winnerTr").empty();
+    $("#pageNavi").empty();
+
+    let reqPage = $(this).text();
+    changePage(reqPage);
+
+});
+
+function changePage(reqPage){
+    $.ajax({
+        url : "/event/winnerBoard",
+        type : "get",
+        data : {reqPage : reqPage},
+        success : function(data){
+            for(let i=0 ; i<data.winnerBoardList.length ; i++){
+                const winnerBoard = data.winnerBoardList[i];
+                const tr = $("<tr>");
+                tr.addClass("winnerTr")
+                const noTd = $("<td>");
+                noTd.text(winnerBoard.winNo);
+                const titleTd = $("<td>");
+                titleTd.text(winnerBoard.winTitle);
+                const writerTd = $("<td>");
+                writerTd.text(winnerBoard.winWriter);
+                const regDateTd = $("<td>");
+                regDateTd.text(winnerBoard.regDate);
+                tr.append(noTd).append(titleTd).append(writerTd).append(regDateTd);
+                $(".notice-tbl").children("tbody").append(tr);
+            }
+            $("#pageNavi").append(data.pageNavi);
+        }
+    });
+}
