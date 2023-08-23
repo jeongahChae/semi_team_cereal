ccaf1953ff2ca1c7f95247569c4a5b46beef3162
Donghyo-Son <121651986+Donghyo-Son@users.noreply.github.com>
2023-08-23 13:31:43 +0900

    마이페이지 메뉴 페이지 이동 작업 1

--- src/main/java/kr/or/iei/myPage/controller/MyPageController.java
+++ src/main/java/kr/or/iei/myPage/controller/MyPageController.java
@@ -1,6 +1,7 @@
 package kr.or.iei.myPage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MyPageController {
 	
 	@GetMapping(value="/updateMemberInfo")
-	public String updateMemberInfo() {
+	public String updateMemberInfo(int btn, Model model) {
+		model.addAttribute("btn", btn);
 //      return "myPage/Donghyo_orderHistory-deliveryStatus_1";
 //      return "myPage/Donghyo_orderHistory-deliveryStatus_2";
 //      return "myPage/Donghyo_orderCancel-change-return_1";
@@ -25,8 +27,7 @@ public class MyPageController {
 //	    return "myPage/Donghyo_myPageList";
 //		return "myPage/Donghyo_accmulated_money";
 		
-		
-		
+
 		return "myPage/updateMemberInfo";
 	}//orderHistoryDeliveryStatus1()
 	
