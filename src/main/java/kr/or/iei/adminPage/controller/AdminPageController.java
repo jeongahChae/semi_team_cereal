1393856ed8b7dba9b2fc7ef0bf515bd8962df338
Donghyo-Son <121651986+Donghyo-Son@users.noreply.github.com>
2023-08-23 15:45:47 +0900

    관리자페이지 메뉴 이동 1차

--- src/main/java/kr/or/iei/adminPage/controller/AdminPageController.java
+++ src/main/java/kr/or/iei/adminPage/controller/AdminPageController.java
@@ -1,19 +1,35 @@
 package kr.or.iei.adminPage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value="adminPage")
+@RequestMapping(value="adminPage") //관리자 페이지
 public class AdminPageController {
     
+	//회원목록
     @GetMapping(value="/allMemberList")
-    public String allMemberListt() {
-//        return "adminPage/Donghyo_adminPageList";
-        return "adminPage/Donghyo_allMemberList";
-//        return "adminPage/Donghyo_allProductList";
-//    	return "adminPage/Donghyo_orderStatusManagement";
-    }//cssTest()
+    public String allMemberListt(int btn, Model model) {
+        model.addAttribute("btn", btn);
+    	return "adminPage/Donghyo_allMemberList";
+    }//allMemberListt(int btn, Model model)
+    
+    
+    //등록 상품 현황
+    @GetMapping(value="/allProductList")
+    public String allProductList(int btn, Model model) {
+    	model.addAttribute("btn", btn);
+        return "adminPage/Donghyo_allProductList";
+    }//allProductList(int btn, Model model)
+    
+    
+    //주문 현황 관리
+    @GetMapping(value="/orderStatusManagement")
+    public String orderStatusManagement(int btn, Model model) {
+    	model.addAttribute("btn", btn);
+    	return "adminPage/Donghyo_orderStatusManagement";
+    }//orderStatusManagement(int btn, Model model)
     
 }//AdminPageController
