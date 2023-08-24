78919995b986891f9000f71834c0e24c8bd6fb21
Donghyo-Son <121651986+Donghyo-Son@users.noreply.github.com>
2023-08-24 10:35:06 +0900

    체크박스 체크 유무를 활용한 접수 버튼 활성화 완성

--- src/main/resources/static/javascript/Donghyo_orderCancel-change-return 1.js
+++ src/main/resources/static/javascript/Donghyo_orderCancel-change-return 1.js
@@ -1,10 +1,12 @@
 //주문취소/교환/반품 1
 //Donghyo_orderCancel-change-return 1.html
-$("#tableId").on('click', 'tr', function(e){
-    if( $(e.target).is('input:checkbox') ) return;
-    var chkbox = $(this).find('td:first-child :checkbox');
-    chkbox.prop('checked', !chkbox.prop('checked'));
-});
+
+//tr을 눌러도 체크가 됨(미사용)
+// $("#tableId").on('click', tr, function(e){
+//     if( $(e.target).is('input:checkbox') ) return;
+//     var chkbox = $(this).find('td:first-child :checkbox');
+//     chkbox.prop('checked', !chkbox.prop('checked'));
+// });
 
 
 //체크박스가 체크되었을 때만 '접수'버튼 활성화
@@ -14,20 +16,33 @@ $("#tableId").on('click', 'tr', function(e){
         1-2. 체크박스가 체크 상태인지, 아닌지 판별
     2. 체크박스가 체크되어 있으면 '접수'버튼 활성화
         2-1. '접수'버튼 인식 -> js에서 변수로 선언
-    3. <input>의 체크박스 값이 false이면 <button>태그 내부의
-        링크값을 빼고, 체크박스 값이 true이면 링크값을 텍스트로 
-        삽입
+    3. 체크박스를 체크하면 '접수'버튼 생성, 체크 비활성화시, '접수'버튼 삭제
 */
+
+// const registerBtn = document.querySelector(".registerBtn"); //2-1
+// registerBtn.addEventListener("click", function(){
+//     console.log("접수 버튼 클릭");
+// });
+
+//3
+function getLink(){
+    const registerBtnWrap  = document.getElementsByClassName("registerBtn-wrap")[0];
+    registerBtnWrap.innerHTML = "<button type='button' class='registerBtn' onclick=\"window.location.href=\'/myPage/Donghyo_orderCancel-change-return_2?btn=2\';\">접수</button>";
+}
+function removeLink(){
+    const registerBtn = document.querySelector(".registerBtn"); 
+    registerBtn.parentNode.innerHTML = "";
+}
+
 const checkbox = document.querySelector("#check1"); //1-1
 checkbox.addEventListener("click", function(){
     if(checkbox.checked == true){
-        console.log("체크 활성화 "+checkbox.checked);
+        // console.log("체크 활성화 "+checkbox.checked);
+        getLink();
     }else if(checkbox.checked == false){
-        console.log("체크 비활성화 "+checkbox.checked);
+        // console.log("체크 비활성화 "+checkbox.checked);
+        removeLink();
     }
 }); //1-2
-const registerBtn = document.querySelector(".registerBtn"); //2-1
-registerBtn.addEventListener("click", function(){
-    console.log("접수 버튼 클릭");
-});
+
 
