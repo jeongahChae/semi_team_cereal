c08971d56b3c3e998133e3f907bf95db8c8ab52b
psj <142005747+gyeongeunpark@users.noreply.github.com>
2023-08-29 18:03:18 +0900

    자주하는 질문 완료

--- src/main/resources/static/javascript/usualQna.js
+++ src/main/resources/static/javascript/usualQna.js
@@ -1,47 +1,59 @@
 
+/* usualQnaList.html */
+//tr 클릭 시 slide되는 용도  <- 만약 시간이 된다면 tr이랑 text랑 각각 slideDown과 fadeOut을 줘서 디자인해보기
+$("tr.showContent").click(function() {
+    //먼저 전체 슬라이드를 올려주는 코드
+    const allContent = $(".qnaContent")
+    allContent.fadeOut();
+    // allContent.slideUp();
+    //내가 원하는 컨텐츠만 내려주는 코드
+    const usualQnaContent = $(this).next().find(".qnaContent");
+    //컨텐츠가 visible(slideDown상태)인 경우
+    if(usualQnaContent.is(':visible')){
+        // usualQnaContent.slideUp();
+        // usualQnaContent.fadeOut();
+        //  
+    }else{
+        usualQnaContent.fadeIn();
+        // usualQnaContent.slideDown();
+    }
+});
 
-/* noticeView.html */
 //삭제버튼 -> 시간이 난다면 모달창으로 처리하기
-function noticeDelete(noticeNo){
+function usualQnaDelete(qnaNo){
     if(confirm("게시글을 삭제하시겠습니까?")){
-        location.href = "/notice/delete?noticeNo="+noticeNo;
+        location.href = "/usualQna/delete?qnaNo="+qnaNo;
     }
 }
 
 
-/* nituceWriteFrm.html */
+/* usualQnaWriteFrm.html */
 //등록버튼(모달용)
 function checkValue(){
     const titleInput = $("#writeFrmTitle").val();
     const contentInput = $("#writeFrmContent").val();
+    const categoryInput = $("#writeFrmCategory").val();
+
     if((titleInput.trim() === "")||(contentInput.trim() === "")){
         //모달창 띄우기
-        $(".modal").css("display","flex")
+        $("#writeFrmModal").css("display","flex");
         return false;
+    }else{
+        if((categoryInput.trim() === "")){
+            $("#writeFrmModal2").css("display","flex");
+            return false;
+        }
     }
 }
+
 //모달창 닫는 용도
 $("#closeModal").on("click",function(){
-    $(".modal").css("display","none");
+    $("#writeFrmModal").css("display","none");
 });
-//파일 업로드 할 시 작은 이미지 옆으로 생성(미리보기)
-$("#photo-picker").on("change",function(){
-    //첨부파일은 multiple옵션 사용 시 여러개일 수 있으므로 배열로 처리
-    //첨부파일 갯수가 0개가 아니고 && 첫번째 파일이 정상이면
-    if(this.files.length != 0 && this.files[0] != 0){
-        const reader = new FileReader();//파일정보를 얻어올 수 있는 객체
-        //선택한 파일 정보를 읽어옴(비동기처리)
-        reader.readAsDataURL(this.files[0]);
-        //파일리더가 정보를 다 읽어오면 동작할 함수
-        reader.onload = function(e){
-            $(".photo-img").attr("src",e.target.result);
-            $(".img-box-add").css("display","block")
-        }
-    }else{
-        $(".photo-img").attr("src","");
-        $(".img-box-add").css("display","none");
-    }
+$("#closeModal2").on("click",function(){
+    $("#writeFrmModal2").css("display","none");
 });
+
 //summernote(editor기능 추가)
 $("#writeFrmContent").summernote({
     height: 400,
@@ -81,18 +93,16 @@ function uploadImage(file,editor){
     //contentType : enctype="application/x-www-form-urlencoded;charSset=UTF-8"
     // -> 파일전송을 위해서 enctype 기본값 제거
 }
+$(function(){
+    const qnaCategoryVal = $(".usualQna-category").attr("data");
+    const option = $(".usualQna-category").children();
+    console.log(qnaCategoryVal);
+    for(let i=0;i<option.length;i++){
+        const optionVal = option.eq(i).val();
+        if(qnaCategoryVal == optionVal){
+            option.eq(i).prop("selected",true);
+            break;
+        }
 
-/* updateFrm.html */
-//파일 삭제
-function deleteFile(obj,fileNo){
-    if(confirm("파일을 삭제하시겠습니까?")){
-        const input = $("<input>");
-        input.attr("name","delFileNo");
-        input.attr("type","hidden");
-        input.val(fileNo);
-        $(".noticeUpdateFrm").append(input);
-        $(obj).parent().remove();
     }
-}
-
-
+});
