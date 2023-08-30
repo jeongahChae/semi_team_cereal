a09fa731eaf9095e5180b583f265505379992031
ChaeYeon <142197049+kym0416@users.noreply.github.com>
2023-08-30 16:35:09 +0900

    상세페이지

--- src/main/resources/static/javascript/productDetail.js
+++ src/main/resources/static/javascript/productDetail.js
@@ -1,6 +1,6 @@
 $(function(){
     $(".tab>ul>li").eq(0).addClass("active-tab");
-    $(".product-detail").addClass("active-content");
+    $(".product-detail-img").addClass("active-content");
     $(".tab>ul>li").eq(1).addClass("active-tab2");
     $(".tab>ul>li").eq(2).addClass("active-tab2");
     $(".tab>ul>li").eq(3).addClass("active-tab2");
@@ -49,7 +49,54 @@ select.change(function(){
 	    }
 	    $(".count").text(Number(currNum)-1);
 	});
+    let sum = 0;
+    $('.count').each(function(){
+        var text = $(this).text();
+        let text2 = (Number)(text);
+        sum += text2;
+        
+    });
+    let finalPrice = $(".product-detail-info-final-price2").val();
+    let totalPrice = sum * finalPrice;
+    console.log(totalPrice.toLocaleString());
+    $(".info-totalPrice").text(totalPrice.toLocaleString()+"원");
 });
+
+const select2 = $(".product-option2")
+select2.change(function(){
+    const div = $("<div>");
+    const optionDetail = div.addClass("option-detail-box").text($(this).val());
+    
+    const optionCount = $("<div class='option-count2'>");
+    const plusButton = $("<button type='button' id='plus2'>+</button>");
+    const optionPlus = optionCount.append(plusButton);
+    const count = $("<div class='count2'>1</div>");
+    const optionPlusCount = optionPlus.append(count);
+    const optionMinus = $("<button type='button' id='minus2'>-</button>");
+    const optionCountbox = optionPlusCount.append(optionMinus);
+    $(".product-detail-info-optionbox2").css("display","block");
+    $(".product-detail-info-optionbox2").append(optionDetail);
+    $(".product-detail-info-optionbox2").append(optionCountbox);
+    
+    $("#plus2").on("click",function(){
+	    const currNum = $(".count2").text();
+	    $(".count2").text(Number(currNum)+1);
+	});
+	
+	$("#minus2").on("click",function(){
+	    const currNum = $(".count2").text();
+	    if(Number(currNum) == 1){
+	        alert("수량은 1개 이상만 가능합니다.");
+	        return;
+	    }
+	    $(".count2").text(Number(currNum)-1);
+	});
+});
+
+
+
+
+
 
 
 
