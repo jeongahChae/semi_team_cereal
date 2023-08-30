a99990d07b5332494dbe102f6c785c0584109cc8
wngpodong <142201273+wngpodong@users.noreply.github.com>
2023-08-31 02:30:58 +0900

    약관 수정

--- src/main/resources/static/javascript/header_default.js
+++ src/main/resources/static/javascript/header_default.js
@@ -1,25 +1,13 @@
-$(function () {
-  $(document).on("click", ".modal-open-btn", function () {
-    console.log(1);
-    $($(this).attr("target")).css("display", "flex");
-  });
+$(function () {  
   $(document).on("click", ".modal-close", function () {
     console.log(2);
     $(this).parents(".modal-wrap").parent().css("display", "none");
   });  
-  $(".sub-navi").prev().after("<span class='material-icons dropdown'>expand_more</span>");
+  //$(".sub-navi").prev().after("<span class='material-icons dropdown'>expand_more</span>");
 });
  
  $(".modal-open-btn").click(function(){
     console.log(3);
-    $(".modal-bg").fadeIn(500);
-  
+    $(".modal-bg").fadeIn(500);  
   });
   
-  $(document).mouseup(function (e){
-    console.log(4);
-    if($(".modal").has(e.target).length === 0){
-      $(".modal").fadeOut(500);
-    }
-  });
-
