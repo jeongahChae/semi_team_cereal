1a4d800a012b547e89756f385b3362f56c2d5e14
ChaeYeon <142197049+kym0416@users.noreply.github.com>
2023-08-31 16:09:43 +0900

    ㅠ

--- src/main/resources/static/javascript/productDetail.js
+++ src/main/resources/static/javascript/productDetail.js
@@ -60,22 +60,28 @@ select.change(function(){
     const optionDetail = div.addClass("option-detail-box").text($(this).val());
     
     const optionCount = $("<div class='option-count'>");
-    const plusButton = $("<button type='button' id='plus'>+</button>");
+    const plusButton = $("<button type='button' class='plus'>+</button>");
     const optionPlus = optionCount.append(plusButton);
     const count = $("<div class='count'>1</div>");
     const optionPlusCount = optionPlus.append(count);
-    const optionMinus = $("<button type='button' id='minus'>-</button>");
+    const optionMinus = $("<button type='button' class='minus'>-</button>");
     const optionCountbox = optionPlusCount.append(optionMinus);
     $(".product-detail-info-optionbox").css("display","block");
     $(".product-detail-info-optionbox").append(optionDetail);
     $(".product-detail-info-optionbox").append(optionCountbox);
     
-    $("#plus").on("click",function(){
-        const currNum = $(".count").text();
-        $(".count").text(Number(currNum)+1);
-	});
+    const countList = $(".count");
+
+
+    $(".plus").each(function(i, item){
+        $(item).on("click",function(){
+            const currNum = $(item).next().text();
+            $(item).next().text(Number(currNum)+1);
+        });
+    });
+    
 	
-	$("#minus").on("click",function(){
+	$(".minus").on("click",function(){
         const currNum = $(".count").text();
         if(Number(currNum) == 1){
             alert("수량은 1개 이상만 가능합니다.");
